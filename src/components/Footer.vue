f0058c4f2cdcedbf2e58d5bc95041c9ba1c9a33c
ssamko <teacher.ssamko@gmail.com>
2021-06-22 19:17:04 +0900

    add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class="footer mt-auto py-3">
+    <div class="container">
+      <h5>
+        <a href="mailto:ssamko@kakao.com">
+          <i>ssamko@kakao.com</i>
+        </a>
+      </h5>
+      <p>
+        Copyright &#9400;
+        <em>이은섭</em>
+        2020 - {{ new Date().getFullYear()}}. All Rights Reserved.
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
