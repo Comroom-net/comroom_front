40b15110ee53fb0a9e5dfaed52c063d4b6b963c1
ssamko <teacher.ssamko@gmail.com>
2021-06-22 19:48:00 +0900

    add kakao ad

---
+++ src/components/kakao_ad.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <ins
+      class="kakao_ad_area"
+      style="display:none;"
+      data-ad-unit="DAN-1h82khgzclbad"
+      data-ad-width="320"
+      data-ad-height="100"
+    ></ins>
+    <script type="text/javascript" src="//t1.daumcdn.net/kas/static/ba.min.js" async></script>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Kakao-ad"
+};
+</script>
