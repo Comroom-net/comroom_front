5274b710d17bb6234ce32a854ea0aebfd96e2080
ssamko <teacher.ssamko@gmail.com>
2021-06-22 19:23:20 +0900

    add howto

---
+++ src/views/howto.vue
@@ -0,0 +1,102 @@
+<template>
+  <article id="35684750-3094-4c7d-a17e-0d4b7b0eeb5c" class="page sans">
+    <div class="col-12 text-center">
+      <figure id="deed06b6-7c90-4c2b-8595-0e2a6c10af1f" class="figure">
+        <img class="img-thumbnail d-block mx-auto figure-img" src="{% static 'howto/1.png' %}" />
+        <figcaption class="figure-caption">학교 컴퓨터실 시간표 관리자를 등록합니다.</figcaption>
+      </figure>
+      <figure id="acf56ea6-d024-4f67-ba30-d6b891fb5509" class="figure">
+        <img class="img-thumbnail d-block mx-auto figure-img" src="{% static 'howto/2.png' %}" />
+        <figcaption class="figure-caption">필수 정보만을 입력해서 가입합니다.</figcaption>
+      </figure>
+      <figure id="cd61ff57-d85d-4c7d-8e1e-a39a13b114bb" class="figure">
+        <img class="img-thumbnail d-block mx-auto figure-img" src="{% static 'howto/3.png' %}" />
+        <figcaption class="figure-caption">
+          가입한 아이디로 로그인하면 링크주소와 버튼이 보입니다. 링크복사 버튼으로
+          복사하셔도 되고 마우스로 직접 복사하셔도 됩니다. 시간표 버튼을 누르면
+          바로 이동합니다.
+        </figcaption>
+      </figure>
+      <figure id="a1572b79-3177-48a6-aa48-3dae0797dce8" class="figure">
+        <img class="img-thumbnail d-block mx-auto figure-img" src="{% static 'howto/10.png' %}" />
+        <figcaption class="figure-caption">
+          메신져로 교내 선생님들께 링크를 안내해주세요~ (학교 코드는 다른 학교에
+          접근을 막기위한 용도로 자동 생성되는 번호 입니다.)
+        </figcaption>
+      </figure>
+      <figure id="f0fe8140-fcac-4e37-b5ae-f6cd997927a9" class="figure">
+        <img class="img-thumbnail d-block mx-auto figure-img" src="{% static 'howto/4.png' %}" />
+        <figcaption class="figure-caption">
+          지난 날은 회색으로, 이미 예약된 시간은 흐리게 표시됩니다. 비어있는
+          시간을 누르면 바로 신청하실 수 있습니다.
+        </figcaption>
+      </figure>
+      <figure class="figure">
+        <img class="img-thumbnail d-block mx-auto figure-img" src="{% static 'howto/14.png' %}" />
+        <figcaption class="figure-caption">예약된 시간을 클릭하면 예약자 정보를 확인할 수 있습니다.</figcaption>
+      </figure>
+      <figure class="figure">
+        <img class="img-thumbnail d-block mx-auto figure-img" src="{% static 'howto/15.png' %}" />
+        <figcaption class="figure-caption">컴퓨터실 버튼을 누르면 다른 교실을 선택할 수 있습니다.</figcaption>
+      </figure>
+      <figure id="626a8288-67e1-4f2d-a4db-938d6617866b" class="figure">
+        <img class="img-thumbnail d-block mx-auto figure-img" src="{% static 'howto/5.png' %}" />
+        <figcaption class="figure-caption">
+          비어있는 시간을 누르면 해당 일자와 시간이 들어가고 학년, 반, 사용하시는
+          선생님 이름만 입력해서 간단하게 예약이 가능합니다.
+        </figcaption>
+      </figure>
+      <h3>관리자 메뉴</h3>
+      <figure class="figure">
+        <img class="img-thumbnail d-block mx-auto figure-img" src="{% static 'howto/11.png' %}" />
+        <figcaption class="figure-caption">관리자 메뉴에서 '컴퓨터실 정보수정' 및 '시간표 수정'을 할 수 있습니다.</figcaption>
+      </figure>
+      <figure class="figure">
+        <img class="img-thumbnail d-block mx-auto figure-img" src="{% static 'howto/12.png' %}" />
+        <figcaption class="figure-caption">
+          컴퓨터실마다 위치, 이용방법, 주의사항 등 설명을 입력할 수 있습니다.
+          컴퓨터실 외의 특별실이나 태블릿 대여 예약 등으로 사용할 수도 있습니다.
+          (사용tip by 솔밭초 허유리 선생님)
+        </figcaption>
+      </figure>
+      <figure class="figure">
+        <img class="img-thumbnail d-block mx-auto figure-img" src="{% static 'howto/13.png' %}" />
+        <figcaption class="figure-caption">
+          잘못 예약된 시간은 관리자가 삭제할 수 있습니다. (더 나은 방법을 위한
+          고민중입니다. 아이디어 있으면 보내주세요.)
+        </figcaption>
+      </figure>
+      <h3 id="2a96b9d4-a375-4e17-baa0-4e484aee369d">꿀팁! - 학교홈페이지에 등록하기</h3>
+      <figure id="cad85df3-f6c4-4cd7-99c4-f3574b5ba522" class="figure">
+        <img class="img-thumbnail d-block mx-auto figure-img" src="{% static 'howto/6.png' %}" />
+        <figcaption class="figure-caption">저렇게 퀵메뉴에 등록해놓으면 선생님들이 쉽게 접근할 수 있습니다.</figcaption>
+      </figure>
+      <figure id="bdca76cc-e879-4387-9042-9893cbe13d83" class="figure">
+        <img class="img-thumbnail d-block mx-auto figure-img" src="{% static 'howto/7.png' %}" />
+        <figcaption class="figure-caption">홈페이지 관리자로 로그인하셔서 &#x27;퀵메뉴관리&#x27;로 갑니다.</figcaption>
+      </figure>
+      <figure id="1d8e2552-ca95-491c-99b3-971f84f335e4" class="figure">
+        <img class="img-thumbnail d-block mx-auto figure-img" src="{% static 'howto/8.png' %}" />
+        <figcaption class="figure-caption">우측 하단에 &#x27;쓰기&#x27;버튼을 눌러서 등록합니다.</figcaption>
+      </figure>
+      <figure id="9be30e46-14a0-4140-81d8-4f6f7bf970b0" class="figure">
+        <img class="img-thumbnail d-block mx-auto figure-img" src="{% static 'howto/9.png' %}" />
+        <figcaption class="figure-caption">위와 같이 아까 복사한 url을 등록해주세요</figcaption>
+      </figure>
+      <p>
+        샘플 계정으로 체험해보고 싶다면
+        <a
+          href="/school/ex_login/"
+          class="btn btn-info btn-sm"
+          role="button"
+        >샘플 계정 로그인</a>버튼을 눌러서 살펴보세요.
+      </p>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  name: "Howto"
+};
+</script>
