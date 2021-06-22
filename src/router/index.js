e39080bb73e70bac4b3c777c20c79a0587dedc15
ssamko <teacher.ssamko@gmail.com>
2021-06-22 19:17:16 +0900

    add about

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import home from '@/components/home'
+import home from '@/views/home'
+import About from '@/views/about'
 
 Vue.use(Router)
 
 export default new Router({
+    mode: 'history',
     routes: [{
-        path: '/',
-        name: 'home',
-        component: home
-    }]
+            path: '/',
+            name: 'home',
+            component: home
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: About
+
+        },
+
+    ]
 })
---
+++ src/views/about.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="row mt-5">
+    <div class="col-lg-4 mx-auto text-center">
+      <img
+        src="profile_persona.jpg"
+        alt="SSAMKO"
+        class="rounded-circle"
+        height="240px"
+        width="240px"
+      />
+
+      <div class="row mt-5">
+        <p class="mx-auto">
+          <a class="about_link" href="http://instagram.com/ssam_ko">
+            <img src="insta_logo.png" height="35px" alt="Instagram" /> @ssam_ko
+          </a>
+          <br />
+          <br />
+          <a class="about_link" href="http://youtube.com/c/schoolswat">
+            <img src="youtube_logo.png" height="35px" alt="Youtube" /> 섭섭미디어
+          </a>
+          <br />
+          <br />
+          <a class="about_link" href="http://ssamko.tistory.com">
+            <img src="tistory_logo.png" height="35px" alt="tistory" /> 블로그(티스토리)
+          </a>
+          <br />
+          <br />
+          <a class="about_link" href="http://qr.kakao.com/talk/koNBb4lY2cSYZ2jkMeAI.ir1KO0-">
+            <img src="kakao_logo.png" height="35px" alt="kakaotalk" /> ssamko
+          </a>
+          <br />
+          <br />
+          <span class="h4">(충북)삼양초등학교 ~2020.5.25.</span>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "about"
+};
+</script>
