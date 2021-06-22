d919fa5a2de4dc5e2823a5817973e8d112fbed1c
ssamko <teacher.ssamko@gmail.com>
2021-06-22 19:36:23 +0900

    add whatis

---
+++ src/views/whatis.vue
@@ -0,0 +1,32 @@
+<template>
+  <article>
+    <div class="col-12 text-center">
+      <br />
+      <h4>컴퓨터실 쓸 때마다~</h4>
+      <br />
+      <figure class="figure">
+        <img class="img-thumbnail d-block mx-auto figure-img" src="{% static 'whatis/2.png' %}" />
+        <figcaption class="figure-caption">눈치게임</figcaption>
+      </figure>
+      <figure class="figure d-none d-sm-block">
+        <img class="img-thumbnail mx-auto figure-img" src="{% static 'whatis/1.png' %}" />
+        <figcaption class="figure-caption">혹은 허락구하기</figcaption>
+      </figure>
+      <figure class="figure">
+        <img class="img-thumbnail d-block mx-auto figure-img" src="{% static 'whatis/3.png' %}" />
+        <figcaption class="figure-caption">나는 안쓸건데 자꾸 귀찮게 날라오는 쪽지들</figcaption>
+      </figure>
+      <br />
+      <h3>컴퓨터실 예약관리 사이트로 해결하세요!</h3>
+      <a href="/howto">
+        <h5>사용방법 보러가기</h5>
+      </a>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  name: "Whatis"
+};
+</script>
