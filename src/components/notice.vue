b5c35701bde09fc29fbcd4cddd023c35c0fcbaa5
ssamko <teacher.ssamko@gmail.com>
2021-06-24 22:21:14 +0900

    add notice frame

---
+++ src/components/notice.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h3>공지사항</h3>
+    <div class="row mt-5">
+      <div class="col-12">
+        <ul>
+          <!-- {% for notice in notices %} -->
+          <li>
+            <!-- {{notice.context|safe}} -->
+            <!-- <small class="text-muted">- {{notice.last_update|date:"y.n.j"}}</small> -->
+            hi
+          </li>
+          <!-- {% endfor %} -->
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Notice"
+};
+</script>
