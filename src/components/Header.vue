7c36ade234d59644292d9587f5680bc208db61a6
teacherSsamko <ssamko@kakao.com>
2021-07-10 22:16:20 +0900

    component:Header: apply bootstrap navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,53 +1,26 @@
 <template>
   <header>
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top" style="background-color: #36802d">
-      <a class="navbar-brand" href="/">컴룸닷넷</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
+    <b-navbar
+      toggleable="lg"
+      class="navbar navbar-expand-md navbar-dark fixed-top"
+      style="background-color: #36802d"
+    >
+      <b-navbar-brand href="/">컴룸닷넷</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item {% block nav-home %}{% endblock %}">
-            <a class="nav-link" href="/">HOME</a>
-          </li>
-          <li class="nav-item {% block nav-about %}{% endblock %}">
-            <a class="nav-link" href="/about">Made by</a>
-          </li>
-          <li class="nav-item {% block nav-whatis %}{% endblock %}">
-            <a class="nav-link" href="/whatis">What is?</a>
-          </li>
-          <li class="nav-item {% block nav-howto %}{% endblock %}">
-            <a class="nav-link" href="/howto">How to</a>
-          </li>
-          <li class="nav-item {% block nav-faq %}{% endblock %}">
-            <a class="nav-link" href="/FAQ">FAQ</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a
-              class="nav-link dropdown-toggle"
-              href="#"
-              id="navbarDropdown"
-              role="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >ETC</a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="/etc/nocookie">유튜브nocookie</a>
-              <a class="dropdown-item" href="/etc/g-suite">G-suite</a>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </nav>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="/" active>HOME</b-nav-item>
+          <b-nav-item href="/about">Made by</b-nav-item>
+          <b-nav-item href="/whatis">What is?</b-nav-item>
+          <b-nav-item href="/howto">How to</b-nav-item>
+          <b-nav-item href="/FAQ">FAQ</b-nav-item>
+          <b-nav-item-dropdown text="ETC">
+            <b-dropdown-item href="/etc/nocookie">유튜브nocookie</b-dropdown-item>
+            <b-dropdown-item href="#" disabled>G-suite</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
   </header>
 </template>
