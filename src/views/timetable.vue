b2ea1e3996ddf8f3b1a1dcddcdbfb437ddaeac39
teacherSsamko <ssamko@kakao.com>
2021-07-27 07:20:48 +0900

    views:timetable: accept parameter in props

--- src/views/timetable.vue
+++ src/views/timetable.vue
@@ -9,6 +9,20 @@ export default {
   name: "Timetable",
   components: {
     Calendar
+  },
+  props: {
+    school: {
+      type: String,
+      default: "school"
+    },
+    s_code: {
+      type: String,
+      default: "1111"
+    }
+  },
+  mounted() {
+    this.$log.debug(this.school);
+    this.$log.debug(this.s_code);
   }
 };
 </script>
