32dd0b94a08cb7dd57117ed1a9a87c87c915b891
teacherSsamko <ssamko@kakao.com>
2021-07-26 22:09:06 +0900

    components:home:active_home: fix url & dropdown menu

--- src/components/home/active_home.vue
+++ src/components/home/active_home.vue
@@ -1,26 +1,14 @@
 <template>
   <div>
-    <p>http://comroom.net/timetable?school={{school|urlencode}}&s_code={{s_code}}</p>
-    <a
-      href="#/timetable?school={{school|urlencode}}&s_code={{s_code}}"
-      class="btn btn-primary"
-      role="button"
-    >시간표</a>
+    <p>http://comroom.net/timetable?school={{encodeURI(school)}}&s_code={{s_code}}</p>
+    <a :href="timetable_url" class="btn btn-primary" role="button">시간표</a>
     <button class="copy btn btn-primary">링크 복사</button>
     <div class="dropdown" style="display:inline-block;">
-      <button
-        class="btn btn-primary dropdown-toggle"
-        type="button"
-        id="dropdownMenuButton"
-        data-toggle="dropdown"
-        aria-haspopup="true"
-        aria-expanded="false"
-      >관리자 메뉴</button>
-      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <a class="dropdown-item" href="comroom_admin/">컴퓨터실 정보 수정</a>
-        <a class="dropdown-item" href="timetable/time_admin/">시간표 수정</a>
-        <a class="dropdown-item" href="timetable/fix_time/">고정시간 등록</a>
-      </div>
+      <b-dropdown variant="primary" class="m-2" id="admin_menu" text="관리자메뉴">
+        <b-dropdown-item class="dropdown-item" href="comroom_admin/">컴퓨터실 정보 수정</b-dropdown-item>
+        <b-dropdown-item class="dropdown-item" href="timetable/time_admin/">시간표 수정</b-dropdown-item>
+        <b-dropdown-item class="dropdown-item" href="timetable/fix_time/">고정시간 등록</b-dropdown-item>
+      </b-dropdown>
     </div>
     <a href="school/logout/" class="btn btn-secondary" role="button">로그아웃</a>
   </div>
@@ -32,7 +20,9 @@ export default {
   data() {
     return {
       school: "펜하",
-      s_code: "1111"
+      s_code: "1111",
+      timetable_url:
+        "#/timetable?school={{encodeURI(school)}}&s_code={{s_code}}"
     };
   }
 };
