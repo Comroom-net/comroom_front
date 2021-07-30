3b9fd5fe38341ae0aae26bec4f29eee502f9c1bf
ssamko <teacher.ssamko@gmail.com>
2021-07-30 19:00:03 +0900

    views:school:user_active: add timed routing

--- src/views/school/user_active.vue
+++ src/views/school/user_active.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>{{message}}</div>
+  <div>
+    <div class="message">{{message}}</div>
+    <div>{{ timer }}초 후 메인페이지로 이동합니다.</div>
+  </div>
 </template>
 
 <script>
@@ -12,16 +15,22 @@ export default {
   },
   data() {
     return {
-      message: ""
+      message: "",
+      timer: 3
     };
   },
   mounted() {
     api.user_active(this.token, this);
   },
   methods: {
+    afterResponse() {
+      setInterval(() => this.timer--, 1000);
+      setTimeout(() => this.$router.push("/"), 3000);
+    },
     active_good(response) {
       this.$log.debug(response);
       this.message = "인증되었습니다. 불편한 사항은 언제든 말씀해주세요 ^^";
+      this.afterResponse();
     },
     active_bad(err_code) {
       this.$log.debug(err_code);
@@ -30,6 +39,7 @@ export default {
         501: "만료된 링크입니다. 다시 가입을 신청하세요"
       };
       this.message = ERROR_MESSAGES[err_code];
+      this.afterResponse();
     }
   }
 };
