a8f8fbb2ebfab095a51301e48b6b72107f91a137
teacherSsamko <ssamko@kakao.com>
2021-08-08 00:47:27 +0900

    active_home: change bootstrap to vuetify

--- src/components/home/active_home.vue
+++ src/components/home/active_home.vue
@@ -1,13 +1,17 @@
 <template>
-  <div>
-    <div class="dropdown" style="display:inline-block;">
-      <b-dropdown variant="primary" class="m-2" id="admin_menu" text="관리자메뉴">
-        <b-dropdown-item class="dropdown-item" href="comroom_admin/">컴퓨터실 정보 수정</b-dropdown-item>
-        <b-dropdown-item class="dropdown-item" href="timetable/time_admin/">시간표 수정</b-dropdown-item>
-        <b-dropdown-item class="dropdown-item" href="timetable/fix_time/">고정시간 등록</b-dropdown-item>
-      </b-dropdown>
-    </div>
-    
+  <div class="text-center">
+    <v-menu offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn v-bind="attrs" v-on="on">관리자 메뉴</v-btn>
+      </template>
+      <v-list>
+        <v-list-item v-for="(item, index) in adminItems" :key="index">
+          <v-list-item-title>
+            <router-link :to="item.path">{{ item.name }}</router-link>
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
   </div>
 </template>
 
@@ -15,7 +19,22 @@
 export default {
   name: "activeHome",
   data() {
-    return {};
+    return {
+      adminItems: [
+        {
+          name: "컴퓨터실 정보 수정",
+          path: "login"
+        },
+        {
+          name: "시간표 수정",
+          path: "timetable/time_admin"
+        },
+        {
+          name: "고정시간 등록",
+          path: "timetable/fix_time/"
+        }
+      ]
+    };
   }
 };
 </script>
