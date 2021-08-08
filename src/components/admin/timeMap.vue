73ca7faa9862a2ea42750a4d1e24ad36116edeb0
teacherSsamko <ssamko@kakao.com>
2021-08-08 19:58:51 +0900

    timeMap.vue: init

--- src/components/admin/timeMap.vue
+++ src/components/admin/timeMap.vue
@@ -1,9 +1,31 @@
 <template>
-  <div>fixed</div>
+  <div>
+    <v-menu offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn v-bind="attrs" v-on="on">
+          {{selectTimes[startTime]}}
+          <v-icon>mdi-menu-down</v-icon>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item-group v-model="startTime" mandatory>
+          <v-list-item v-for="(time, index) in selectTimes" :key="index">
+            <v-list-item-title>{{ time }}</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-menu>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "TimeMap"
+  name: "TimeMap",
+  data() {
+    return {
+      startTime: 2,
+      selectTimes: ["08:40", "08:50", "09:00", "09:10"]
+    };
+  }
 };
 </script>
