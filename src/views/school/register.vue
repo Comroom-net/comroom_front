446dcf90134152d9cdde268718da085759c7101f
teacherSsamko <ssamko@kakao.com>
2021-08-27 21:18:34 +0900

    school:register: fix email input
    - 교육청 메일 사용불가 안내

--- src/views/school/register.vue
+++ src/views/school/register.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <h1>학교 관리자 등록</h1>
-    
+
     <v-form ref="form" lazy-validation>
       <v-select
         v-model="reqData.province"
@@ -16,8 +16,7 @@
         suffix="초등학교"
         :error-messages="errors.school"
         :rules="[rules.required]"
-      >
-      </v-text-field>
+      ></v-text-field>
       <div v-for="form in forms" :key="form.id">
         <v-text-field
           v-model="reqData[form.name]"
@@ -27,17 +26,10 @@
           :error-messages="errors[form.name]"
           :rules="getRules(form.name)"
           :hint="form.small"
-        >
-        </v-text-field>
+        ></v-text-field>
       </div>
-      <v-btn 
-      color="success"
-      class="mr-4" 
-      @click="registerSubmit">
-      등록</v-btn>
-      <v-btn 
-      text
-      @click="cancel">취소</v-btn>
+      <v-btn color="success" class="mr-4" @click="registerSubmit">등록</v-btn>
+      <v-btn text @click="cancel">취소</v-btn>
     </v-form>
     <v-snackbar v-model="snackbar" :timeout="10000">
       {{error}}
@@ -64,7 +56,7 @@ export default {
         password: null,
         re_password: null,
         email: null,
-        realname: null,
+        realname: null
       },
       rules: {
         required: value => !!value || "필수입력란",
@@ -72,7 +64,7 @@ export default {
           const pattern = /^[0-9]+$/;
           return pattern.test(value) || "숫자를 입력해주세요.";
         },
-        counter: value => value && value.length <= 20 || "최대 20자",
+        counter: value => (value && value.length <= 20) || "최대 20자",
         email: value => {
           const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
           return pattern.test(value) || "Invalid e-mail.";
@@ -104,33 +96,35 @@ export default {
           label: "교내 컴퓨터실 수",
           type: "number",
           name: "ea",
-          small: "특별실(스마트패드)을 포함한 수. 최대 5개. 더 필요한 경우 연락주세요.",
+          small:
+            "특별실(스마트패드)을 포함한 수. 최대 5개. 더 필요한 경우 연락주세요."
         },
         {
           label: "아이디",
           type: "text",
-          name: "user",
+          name: "user"
         },
         {
           label: "비밀번호",
           type: "password",
-          name: "password",
+          name: "password"
         },
         {
           label: "비밀번호 확인",
           type: "password",
-          name: "re_password",
+          name: "re_password"
         },
         {
           label: "담당자 이름",
           type: "text",
-          name: "realname",
+          name: "realname"
         },
         {
           label: "이메일",
           type: "email",
           name: "email",
-          small: "비밀번호 분실시 사용됩니다.",
+          small:
+            "교육청(혹은 공무원)메일로는 메일을 받지 못합니다. 비밀번호 분실시 사용됩니다."
         }
       ],
       reqData: {
@@ -142,30 +136,34 @@ export default {
         re_password: null,
         realname: null,
         email: null
-      },
+      }
     };
   },
   methods: {
     cancel() {
-      this.$router.push("/")
+      this.$router.push("/");
     },
     getRules(name) {
       const rules = {
         ea: [this.rules.required, this.rules.number, this.rules.ea],
         user: [this.rules.required, this.rules.counter],
         password: [this.rules.required],
-        re_password: [this.rules.required, (this.reqData.password === this.reqData.re_password) || '비밀번호가 일치하지 않습니다.'],
+        re_password: [
+          this.rules.required,
+          this.reqData.password === this.reqData.re_password ||
+            "비밀번호가 일치하지 않습니다."
+        ],
         realname: [this.rules.required, this.rules.counter],
         email: [this.rules.required, this.rules.email]
-      }
-      return rules[name]
+      };
+      return rules[name];
     },
     registerSubmit() {
       if (this.$refs.form.validate()) {
         this.$log.debug(this.reqData);
         api.register(this.reqData, this);
       }
-    },
+    }
   }
 };
 </script>
