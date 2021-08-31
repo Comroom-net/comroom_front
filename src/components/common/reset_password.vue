f1df7366b5154f1db978df19ffb26c0736d92cf9
ssamko <teacher.ssamko@gmail.com>
2021-08-31 22:10:21 +0900

    components: common:  init reset_password

--- src/components/common/reset_password.vue
+++ src/components/common/reset_password.vue
@@ -1,49 +1,60 @@
 <template>
-  <v-card>
+  <v-card width="500px" class="m-auto mt-10">
     <v-card-title>새 비밀번호 설정</v-card-title>
     <v-card-text>
-      <v-text-field
-        v-model="newPassword"
-        type="password"
-        label="새 비밀번호"
-        :error-messages="passwordError"
-        :rules="getRules('password')"
-      ></v-text-field>
-      <v-text-field
-        v-model="rePassword"
-        type="password"
-        label="비밀번호 확인"
-        :error-messages="passwordError"
-        :rules="getRules('re-password')"
-      ></v-text-field>
+      <v-form ref="form">
+        <v-text-field
+          v-model="newPassword"
+          type="password"
+          label="새 비밀번호"
+          :error-messages="passwordError"
+          :rules="getRules('password')"
+        ></v-text-field>
+        <v-text-field
+          v-model="rePassword"
+          type="password"
+          label="비밀번호 확인"
+          :error-messages="passwordError"
+          :rules="getRules('re_password')"
+        ></v-text-field>
+      </v-form>
     </v-card-text>
-    <v-card-actions></v-card-actions>
+    <v-card-actions>
+      <v-spacer />
+      <v-btn @click="resetPassword">변경</v-btn>
+      <v-spacer />
+    </v-card-actions>
   </v-card>
 </template>
 
 <script>
+import api from "Api/functions/school";
+
 export default {
   name: "ResetPassword",
-  created() {
-    this.$log.debug("reset token:");
-  },
   props: ["token"],
   data() {
     return {
       newPassword: null,
       rePassword: null,
+      passwordError: null,
       rules: {
         required: value => !!value || "필수입력란"
       }
     };
   },
   methods: {
+    resetPassword() {
+      if (this.$refs.form.validate()) {
+        api.resetPassword(this);
+      }
+    },
     getRules(name) {
       const rules = {
         password: [this.rules.required],
         re_password: [
           this.rules.required,
-          this.reqData.password === this.reqData.re_password ||
+          this.newPassword === this.rePassword ||
             "비밀번호가 일치하지 않습니다."
         ]
       };
